f72c32aa756fcfc614d24899402eb3ffa3f8dab4
Krishna Baviskar <krishnabaviskar40@gmail.com>
2025-12-12 14:20:52 +0530

    Update index.ts

--- src/ai/flows/index.ts
+++ src/ai/flows/index.ts
@@ -1,6 +1,6 @@
 export * from './eco-gpt-tutor-action-suggestions';
 export * from './eco-gpt-tutor-initial-explanation';
 export * from './validate-sustainable-actions';
-export * from './generate-quiz';
+export * from './generate-quiz'; 
 export * from './predict-ecoscore';
 export * from './generate-challenges';
