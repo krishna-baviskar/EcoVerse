249cd94d0f7be9ef183412721e9311aeb86ca265
Krishna Baviskar <krishnabaviskar40@gmail.com>
2025-12-12 13:45:22 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+export const firebaseConfig = { 
   "projectId": "studio-1688314080-3520d",
   "appId": "1:766413014148:web:26f1907941afbd77f56ed1",
   "apiKey": "AIzaSyDhHtZ2V7ySmGQfFJhPCniP1QFr9DgzQuk",
